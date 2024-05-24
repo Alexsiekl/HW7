809d578d20a554292ac93f2a7743b6b4440aa89a
Alexsiekl <Alexsiekl@yandex.ru>
2024-05-24 14:37:58 +0300

    Сделал первое 7 урока

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
@@ -6,7 +8,17 @@ public class Main {
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
-
+        boolean flag = true;
+        System.out.println("*** Вас приветствует программа приготовления эликсиров ***");
+        System.out.println("Ведите количество ингридиентов:");
+        System.out.println("Количество жабьих глаз:");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.println("Количество слёз вурдалака:");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.println("Количество костей ворона:");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.println("Количество пельменей:");
+        dumplingsCount = new Scanner(System.in).nextInt();
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
         // 2. 2 кости ворона + 4 пельменя - Эликсир стойкости
@@ -14,5 +26,26 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+        while (flag) {
+            if (toadEyesCount >= 3 && ravenBonesCount >= 1) {
+                System.out.println("Вы можете приготовить эликсир зоркости");
+                if (ravenBonesCount >= 2 && dumplingsCount >= 4) {
+                    System.out.println("Вы можете приготовить эликсир стойкости");
+                    if (toadEyesCount >= 2 && ghoulTearsCount >= 7 && dumplingsCount >= 1) {
+                        System.out.println("Вы можете приготовить эликсир скрытности");
+                        if (toadEyesCount >= 4 && ghoulTearsCount >= 5 && dumplingsCount >= 10 && ravenBonesCount >= 3) {
+                            System.out.println("Вы можете приготовить запретный эликсир");
+                        }
+                    }
+                }
+            } else {
+                System.out.println("У Вас не хватает ингридиентов для приготовления эликсира");
+            }
+                flag = false;
+            }
+        }
     }
-}
+
+
+
+
