2ea00c26c75c1fdc128ca5eaf8c69e33fa5a7c0a
Alexsiekl <Alexsiekl@yandex.ru>
2024-05-30 15:57:39 +0300

    Выполнено 2-е задание 7 урока

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        for (int id = 2; id <= 100; id++){
+            System.out.println("Уволен айтишник с id " + id);
+            id = id + 1;
+        }
     }
 }
